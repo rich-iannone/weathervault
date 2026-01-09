e5c58d80fe46ab4a3838ebdb9c55027640d36ecd
Richard Iannone <riannone@me.com>
2026-01-09 12:14:00 -0500

    Create __init__.py

---
+++ weathervault/data/__init__.py
@@ -0,0 +1,11 @@
+"""Bundled sample data for documentation examples.
+
+This package contains cached weather data files for stations used in
+documentation examples. This allows documentation to be built without
+network access to NOAA servers.
+
+Included stations:
+- 744860-94789 (JFK Airport, New York)
+- 725030-14732 (LaGuardia Airport, New York)
+- 724050-13743 (Washington Reagan Airport)
+"""
